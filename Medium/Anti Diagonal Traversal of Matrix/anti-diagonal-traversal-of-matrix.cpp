c0ef82b9b8a7439d149cb6d84afd4452364b274a
Shashank-GVS <151811408+Shashank-GVS@users.noreply.github.com>
2023-12-27 01:51:50 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Anti Diagonal Traversal of Matrix/anti-diagonal-traversal-of-matrix.cpp
@@ -0,0 +1,56 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    vector<int> antiDiagonalPattern(vector<vector<int>> matrix) 
+    {
+        // Code here
+       vector<int> ans;
+       int n=matrix.size();
+       for(int i=0;i<n;i++){
+           int row=0,col=i;
+           while(col>=0 && row<n){
+               ans.push_back(matrix[row++][col--]);
+           }
+       }
+       for(int i=1;i<n;i++){
+           int row=i,col=n-1;
+           while(row<n && col>=0){
+               ans.push_back(matrix[row++][col--]);
+           }
+       }
+       return ans;
+    }
+};
+
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+
+    while (t--) {
+        int n;
+        cin >> n;
+        vector<vector<int>> matrix;
+        matrix.resize(n, vector<int>(n));
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                cin >> matrix[i][j];
+            }
+        }
+
+        Solution ob;
+        vector<int> ans = ob.antiDiagonalPattern(matrix);
+        for (int i = 0; i < ans.size(); ++i) cout << ans[i] << " ";
+        cout << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
