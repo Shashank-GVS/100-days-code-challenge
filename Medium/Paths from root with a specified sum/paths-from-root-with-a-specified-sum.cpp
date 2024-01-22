6fa2f950980cc157dc2b7a0a0195b7c9c9bbbc57
Shashank-GVS <151811408+Shashank-GVS@users.noreply.github.com>
2024-01-22 16:31:50 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Paths from root with a specified sum/paths-from-root-with-a-specified-sum.cpp
@@ -0,0 +1,179 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include<bits/stdc++.h>
+using namespace std;
+
+// A Tree node
+struct Node
+{
+	int key;
+	struct Node *left, *right;
+};
+
+// Utility function to create a new node
+Node* newNode(int key)
+{
+	Node* temp = new Node;
+	temp->key = key;
+	temp->left = temp->right = NULL;
+	return (temp);
+}
+
+Node* buildTree(string str)
+{
+	// Corner Case
+	if (str.length() == 0 || str[0] == 'N')
+		return NULL;
+
+	// Creating vector of strings from input
+	// string after spliting by space
+	vector<string> ip;
+
+	istringstream iss(str);
+	for (string str; iss >> str; )
+		ip.push_back(str);
+
+	// Create the root of the tree
+	Node* root = newNode(stoi(ip[0]));
+
+	// Push the root to the queue
+	queue<Node*> queue;
+	queue.push(root);
+
+	// Starting from the second element
+	int i = 1;
+	while (!queue.empty() && i < ip.size()) {
+
+		// Get and remove the front of the queue
+		Node* currNode = queue.front();
+		queue.pop();
+
+		// Get the current node's value from the string
+		string currVal = ip[i];
+
+		// If the left child is not null
+		if (currVal != "N") {
+
+			// Create the left child for the current node
+			currNode->left = newNode(stoi(currVal));
+
+			// Push it to the queue
+			queue.push(currNode->left);
+		}
+
+		// For the right child
+		i++;
+		if (i >= ip.size())
+			break;
+		currVal = ip[i];
+
+		// If the right child is not null
+		if (currVal != "N") {
+
+			// Create the right child for the current node
+			currNode->right = newNode(stoi(currVal));
+
+			// Push it to the queue
+			queue.push(currNode->right);
+		}
+		i++;
+	}
+
+	return root;
+}
+
+
+// } Driver Code Ends
+
+//User function Template for C++
+
+/*// A Tree node
+struct Node
+{
+	int key;
+	struct Node *left, *right;
+};*/
+
+class Solution
+{
+    public:
+        void printPathsUtil(Node* curr_node, int sum,
+                     int sum_so_far, vector<int> &path , vector<vector<int>> &ans)
+            {
+            	if (curr_node == NULL)
+            		return;
+            
+            	// add the current node's value
+            	sum_so_far += curr_node->key;
+            
+            	// add the value to path
+            	path.push_back(curr_node->key);
+            
+            	// print the required path
+            	if (sum_so_far == sum )
+            	{
+            		ans.push_back(path);
+            	}
+            
+            	// if left child exists
+            	if (curr_node->left != NULL)
+            		printPathsUtil(curr_node->left, sum, sum_so_far, path, ans);
+            
+            	// if right child exists
+            	if (curr_node->right != NULL)
+            		printPathsUtil(curr_node->right, sum, sum_so_far, path, ans);
+            
+            
+            	// Remove last element from path
+            	// and move back to parent
+            	path.pop_back();
+            }
+
+    public:
+        vector<vector<int>> printPaths(Node *root, int sum)
+            {
+            	vector<int> path;
+            	vector<vector<int>> ans;
+            	printPathsUtil(root, sum, 0, path, ans);
+            	return ans;
+            }
+};
+
+//{ Driver Code Starts.
+
+int main ()
+{
+
+	int t;
+	cin >> t;
+	getchar();
+
+	while (t--)
+	{
+
+		int sum;
+		cin >> sum;
+		getchar();
+
+		string inp;
+		getline(cin, inp);
+
+		struct Node* root = buildTree(inp);
+
+        Solution ob;
+		vector<vector<int>> ans = ob.printPaths(root, sum);
+		sort(ans.begin(),ans.end());
+		for (int i = 0; i < ans.size(); i++)
+		{
+			for (int j = 0; j < ans[i].size(); j++)
+				cout << ans[i][j] << " ";
+
+			cout << "\n";
+		}
+
+	}
+
+	return 0;
+}
+// } Driver Code Ends
